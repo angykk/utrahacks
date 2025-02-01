684377cba6b71de1d9ddb278bfa831dc85be9de3
Hany25 <YitingHan4@gmail.com>
2025-02-01 16:18:18 -0500

    added functions for stuff

--- src/main.h
+++ src/main.h
@@ -12,7 +12,7 @@ const int leftMotor = 0; //digital pin
 const int rightMotor = 1; 
 
 //analog pins
-const int colorSensor = 0; //analog pin
+const int colorSensor = A0; //analog pin
 
 
 //other
@@ -29,25 +29,33 @@ void setup() {
     
 }
 
+
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(leftMotor, speed);
-  digitalWrite(rightMotor, speed);
   
 }
 
 // put function definitions here:
 void goForward(){
   
-  
+  digitalWrite(leftMotor, speed);
+  digitalWrite(rightMotor, speed);
 
 }
 void turnLeft(){
-
+   digitalWrite(leftMotor, 0);
+   digitalWrite(rightMotor, speed);
 }
 void turnRight(){
-
+  digitalWrite(leftMotor, speed);
+  digitalWrite(rightMotor, 0);
 }
-void turn180(){
 
+void turnLeft_FAST(){
+   digitalWrite(leftMotor, -speed);
+   digitalWrite(rightMotor, speed);
+}
+void turnRight_fast(){
+  digitalWrite(leftMotor, speed);
+  digitalWrite(rightMotor, -speed);
 }
