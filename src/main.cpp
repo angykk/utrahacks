9cf58fbf129ea0fc9919944ba6ff3f61fdc5308b
Angela Huang <angelaykhuang@gmail.com>
2025-02-01 17:42:52 -0500

    colour sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,33 @@
 #include <Arduino.h>
+#include <colourSensor.h>
 
 void goForward();
 void turnLeft();
 void turnRight();
 void turn180();
+
+void setup()
+{
+	// Set S0 - S3 as outputs
+	pinMode(S0, OUTPUT);
+	pinMode(S1, OUTPUT);
+	pinMode(S2, OUTPUT);
+	pinMode(S3, OUTPUT);
+
+	// Set Pulse Width scaling to 20%
+	digitalWrite(S0, HIGH);
+	digitalWrite(S1, LOW);
+
+	// Set Sensor output as input
+	pinMode(sensorOut, INPUT);
+
+	// Setup Serial Monitor
+	Serial.begin(9600);
+}
+
+void loop(){
+    getColour();
+}
 
 
 // put function definitions here:
