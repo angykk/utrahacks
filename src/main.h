48efb1e1b08f83e02f88151df67b1039af03b069
Alex J <94213401+alexjiang903@users.noreply.github.com>
2025-02-01 17:46:28 -0500

    initial commit to publish branch

--- src/main.h
+++ src/main.h
@@ -8,21 +8,30 @@ void turn180();
 //PIN OUT (can change later if needed)
 
 //digital pins
-const int leftMotor = 0; //digital pin
-const int rightMotor = 1; 
+const int l_p = 0; //positive pin for left motor
+const int l_n = 1; //negative pin for right motor
+const int r_p = 3; //positive pin for left motor
+const int r_n = 24; //negative pin for right motor 
 
 //analog pins
 const int colorSensor = A0; //analog pin
 
 //other
-const int speed = 127; //range is 0-255 (255 is max speed, 0 is off)
+const int DEFAULT_SPEED = 127; //range is 0-255 (255 is max DEFAULT_SPEED, 0 is off)
 
 
-//Move functions
+
+//test commit 
+//Move functions 
 void setup() {
     // put your setup code here, to run once:
-    pinMode(leftMotor, OUTPUT);
-    pinMode(rightMotor, OUTPUT);
+
+    //motor outputs
+    pinMode(l_p, OUTPUT);
+    pinMode(l_n, OUTPUT);
+    pinMode(r_p, OUTPUT);
+    pinMode(r_n, OUTPUT);
+
     pinMode(colorSensor, INPUT);
     Serial.begin(9600);    
     
@@ -36,25 +45,54 @@ void loop() {
 
 // put function definitions here:
 void goForward(){
-  
-  digitalWrite(leftMotor, speed);
-  digitalWrite(rightMotor, speed);
+  digitalWrite(l_p, HIGH);
+  digitalWrite(l_n, LOW);
+
+  digitalWrite(r_p, HIGH);
+  digitalWrite(r_n, LOW);
 
 }
 void turnLeft(){
-   digitalWrite(leftMotor, 0);
-   digitalWrite(rightMotor, speed);
+  digitalWrite(l_p, HIGH);
+  digitalWrite(l_n, HIGH);
+
+  digitalWrite(r_p, HIGH);
+  digitalWrite(r_n, LOW);
 }
 void turnRight(){
-  digitalWrite(leftMotor, speed);
-  digitalWrite(rightMotor, 0);
+  digitalWrite(l_p, HIGH);
+  digitalWrite(l_n, LOW);
+
+  digitalWrite(r_p, HIGH);
+  digitalWrite(r_n, HIGH);
 }
 
 void turnLeft_FAST(){
-   digitalWrite(leftMotor, -speed);
-   digitalWrite(rightMotor, speed);
+  digitalWrite(l_p, HIGH);
+  digitalWrite(l_n, LOW);
+
+  digitalWrite(r_p, LOW);
+  digitalWrite(r_n, HIGH);
+ 
 }
 void turnRight_fast(){
-  digitalWrite(leftMotor, speed);
-  digitalWrite(rightMotor, -speed);
+  digitalWrite(l_p, LOW);
+  digitalWrite(l_n, HIGH);
+
+  digitalWrite(r_p, HIGH);
+  digitalWrite(r_n, LOW);
+  
+}
+
+void turn180() {
+  // quick turn 180 degrees
+  turnLeft_FAST();
+  delay(1000); //change delay value accordingly (how long it will keep on turning)
+  
+  //stop motors after delay
+  digitalWrite(l_p, LOW);
+  digitalWrite(l_n, LOW);
+
+  digitalWrite(r_p, LOW);
+  digitalWrite(r_n, LOW);
 }
