db7798b005518a004063376bf7627ad34f327316
Alex J <94213401+alexjiang903@users.noreply.github.com>
2025-02-01 14:35:04 -0500

    initial pinout stuff (will merge to main)

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,17 @@ void turnRight();
 void turn180();
 
 //PIN OUT (can change later if needed)
-const int leftMotor = 0;
-const int rightMotor = 1;
 
+//digital pins
+const int leftMotor = 0; //digital pin
+const int rightMotor = 1; 
+
+//analog pins
+const int colorSensor = 0; //analog pin
+
+
+//other
+const int speed = 127; //range is 0-255 (255 is max speed, 0 is off)
 
 
 //Move functions
@@ -16,12 +24,16 @@ void setup() {
     // put your setup code here, to run once:
     pinMode(leftMotor, OUTPUT);
     pinMode(rightMotor, OUTPUT);
-    
+    pinMode(colorSensor, INPUT);
+    Serial.begin(9600);    
     
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(leftMotor, speed);
+  digitalWrite(rightMotor, speed);
+  
 }
 
 // put function definitions here:
