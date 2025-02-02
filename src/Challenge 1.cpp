63a3fea421b23cbebcf4e3dbba49b19228612863
Hany25 <YitingHan4@gmail.com>
2025-02-01 23:13:55 -0500

    stash

--- src/Challenge 1.cpp
+++ src/Challenge 1.cpp
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // #include <Arduino.h>
 // #include <main.h>;
 // #include <colourSensor/colourSensor.h>
@@ -6,43 +7,76 @@
 
 #include <main.h>;
 #include <colourSensor.h>
+=======
+#include <Arduino.h>
+#include <main.h>;
+#include <colourSensor/colourSensor.h>
+>>>>>>> Stashed changes
 
-// unsigned long lastTime = 0;
-// int num = 0;
+unsigned long lastTime = 0;
+int num = 0;
 
+void challenge1()
+{
+    unsigned long currentTime = millis();
+    String colour = getColour();
 
-// void challenge1(){
-//     unsigned long currentTime = millis();
-//     String colour = getColour();
+    turnLeft();
 
-//     turnLeft();
-
-    if (colour == "red" || colour == "green" || colour == "blue"){
+    if (colour == "red" || colour == "green" || colour == "blue")
+    {
         lastTime = millis();
         turnLeft();
     }
 
-//     if (colour != "red" && colour != "green" && colour != "blue"){
-//         stop();
-//         currentTime = millis();
-//     }
+    if (colour != "red" && colour != "green" && colour != "blue")
+    {
+        stop();
+        currentTime = millis();
+    }
 
-//     unsigned long turnLength = currentTime/2;
+    unsigned long turnLength = currentTime / 2;
 
-//     lastTime = millis();
-//     currentTime = millis();
+    lastTime = millis();
+    currentTime = millis();
 
-//     while(currentTime - lastTime < turnLength){
-//         currentTime = millis();
-//         turnRight();
-//     }
+    while (currentTime - lastTime < turnLength)
+    {
+        currentTime = millis();
+        turnRight();
+    }
 
-//     stop();
+    stop();
 
-//     num++;
+    num++;
 
+<<<<<<< Updated upstream
     if (num == 6){
         return;
     }
 }
 */
+=======
+    if (num == 6)
+    {
+        lastTime = millis();
+        currentTime = millis();
+        String cur_colour = colour;
+        while (colour == cur_colour)
+        {
+            goForward();
+        }
+        stop();
+        currentTime = millis();
+        unsigned long turnLength = (currentTime - lastTime) / 2;
+        lastTime = millis();
+        currentTime = millis();
+        while (currentTime - lastTime < turnLength)
+        {
+            currentTime = millis();
+            turnRight();
+        }
+        stop();
+    }
+}
+>>>>>>> Stashed changes
--- src/challenge2.cpp
+++ src/challenge2.cpp
@@ -1,30 +1,37 @@
 #include <main.h>
+#include <colourSensor/colourSensor.h>
+#include <superSonic.h>
 
-unsigned long distance = 0;
+unsigned long distance = 50;
+String colour;
 bool isMoving = false;
-const long length = 28;
-String colour = getColour();
+const long length = 14;
 
-void challenge2(){
+void challenge2()
+{
+    colour = getColour();
+    distance = getDistance();
     goForward();
     isMoving = true;
 
-    if (distance <= length){
+    if (distance < length)
+    {
         stop();
-        if (colour == "blue"){
+        if (colour == "blue")
+        {
             turnLeft_FAST();
         }
-        else if (colour == "green"){
+        else if (colour == "green")
+        {
             turnRight_fast();
         }
-        else if (colour == "black"){
+        else if (colour == "black")
+        {
             stop();
         }
-        else if (colour == "red"){
+        else if (colour == "red")
+        {
             turn180();
         }
     }
-
-
-
 }
