b70a229f1e6bc91c2a9500d2bcce0fdd7b3719d9
Angela Huang <angelaykhuang@gmail.com>
2025-02-02 02:46:50 -0500

    colour

--- src/colourSensor/colourSensor.cpp
+++ src/colourSensor/colourSensor.cpp
@@ -1,8 +1,6 @@
-#pragma once
-
 #include <Arduino.h>
 #include <iostream>
-#include <colourSensor/colourSensor.h>
+#include "colourSensor/colourSensor.h"
 
 int redPW;
 int greenPW;
@@ -73,35 +71,6 @@ String getColour()
 	else{
 		return "Unknown";
 	}
-
-	// Print output to Serial Monitor
-	Serial.print("Red PW = ");
-	Serial.print(redPW);
-	Serial.print(" - Green PW = ");
-	Serial.print(greenPW);
-	Serial.print(" - Blue PW = ");
-	Serial.println(bluePW);
-	// Print output to Serial Monitor
-	Serial.print("Red = ");
-	Serial.print(red);
-	Serial.print(" - Green = ");
-	Serial.print(green);
-	Serial.print(" - Blue = ");
-	Serial.println(blue);
-
-	if (red > green && red > blue) {
-		return "Red";
-	}
-	else if (green > red && green > blue) {
-		return "Green";
-	}
-	else if (blue > red && blue > green) {
-		return "Blue";
-	}
-	else if (red < 100 && green < 100 && blue < 100) {
-		return "Black";
-	}
-
 
 }
 
